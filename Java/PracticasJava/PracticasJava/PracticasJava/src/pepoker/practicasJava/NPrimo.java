1f174cadfe2113baa94a8f9d9185f1f1eda2f606
Jose Ochoa <jochoa@tgc.mx>
2017-03-13 12:15:48 -0700

    Se versiona practica para obtener el N numero primo

---
+++ Java/PracticasJava/PracticasJava/PracticasJava/src/pepoker/practicasJava/NPrimo.java
@@ -0,0 +1,41 @@
+package pepoker.practicasJava;
+
+
+public class NPrimo {
+    private static int getNPrime(int nPrime) {
+        int contPrimo = 0;
+        int num = 1;
+        while (true) {
+
+            if (isPrime(num)) {
+                contPrimo++;
+                if (contPrimo == nPrime)
+                    return num;
+            }
+            num++;
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num == 1)
+            return false;
+
+        if (num == 2)
+            return true;
+
+        if (num % 2 == 0)
+            return false;
+
+        for (int i = 3; i < num; i = i + 2) {
+            if (num % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(getNPrime(10001));
+    }
+
+}
