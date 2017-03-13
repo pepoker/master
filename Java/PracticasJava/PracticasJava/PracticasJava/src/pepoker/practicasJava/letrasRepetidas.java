46b616669305eaaa6d2d739e747eb2138dea6131
Jose Ochoa <jochoa@tgc.mx>
2017-03-13 12:12:16 -0700

    Se versiona practica para determinar si una cadena tiene letras repetidas

---
+++ Java/PracticasJava/PracticasJava/PracticasJava/src/pepoker/practicasJava/letrasRepetidas.java
@@ -0,0 +1,26 @@
+package pepoker.practicasJava;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class letrasRepetidas {
+    public letrasRepetidas() {
+        super();
+    }
+    
+    public static void main(String[] args){
+        System.out.println(tieneLetraRepetida("holo"));
+        System.out.println(tieneLetraRepetida("hola"));
+    }
+    
+    private static boolean tieneLetraRepetida(String palabra){
+        Set<String> setLetras = new HashSet<String>();
+                
+        for (int i =0; i<palabra.length(); i++){
+            if (!setLetras.add(Character.toString(palabra.charAt(i))))
+                return true;
+        }
+        
+        return false;
+    }
+}
